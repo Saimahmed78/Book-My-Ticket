8d4c8dbf1f9319ca6e62c75fc58a77def25c522c
saim ahmed <softwareengineer278@gmail.com>
2026-04-15 22:23:06 +0500

    feat: add isLoggedIn and validateRequest middleware for authentication and validation

---
+++ backend/src/middlewares/isLoggedIn.js
@@ -0,0 +1,30 @@
+import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken"
+const isLoggedIn = async (req, res, next) => {
+   console.log("Checking if user is logged In")
+  // Check if cookies has accessToken
+  const accessToken = req.cookies?.accessToken;
+  
+  // If yes then return next
+  if (accessToken) {
+    try {
+      const decodedData = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      req.user = decodedData;
+    } catch (err) {
+      throw new ApiError(401, "Token is invalid", err);
+    }
+    return next();
+  }
+  console.log("Access Token found sending request to refresh Token")
+  // If not, refresh the access token
+  let decodedRefresh;
+    try {
+      decodedRefresh = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    } catch (err) {
+      throw new ApiError(401, "Refresh Token is invalid");
+    }
+  req.user = decodedRefresh
+  next();
+};
+
+export { isLoggedIn};
